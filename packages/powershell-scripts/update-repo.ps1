1d70cae62b69765fbd25a17a6b9af490a6423a5d
Ethan Glover <hello@ethang.email>
2022-10-26 15:12:04 -0500

    Version Bump

--- packages/powershell-scripts/update-repo.ps1
+++ packages/powershell-scripts/update-repo.ps1
@@ -1,28 +1,6 @@
 Set-Location ~/Projects/ethang-turborepo
 
-$nonWorkspaceLocations = @(
-"apps/website-admin",
-"apps/sterett-admin"
-)
-
-npx nx migrate latest
-
-$migrate = Read-Host "Migrate? (y/n)"
-
-pnpm i
-if ($migrate -eq "y")
-{
-    pnpm nx migrate --run-migrations
-}
-
-pnpm up -i --latest
-
-foreach ($location in $nonWorkspaceLocations)
-{
-    Set-Location ~/Projects/ethang-turborepo/$location
-    pnpm up -i --latest
-}
-Set-Location ~/Projects/ethang-turborepo
+pnpm up -i -r --latest
 
 $commit = Read-Host "Commit? (y/n)"
 
