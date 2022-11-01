0c6fb0967cd64c005a619a23e1c13b05b2def464
Ethan Glover <hello@ethang.email>
2022-10-31 20:09:09 -0500

    rolo

--- apps/monsters-rolodex/src/App.tsx
+++ apps/monsters-rolodex/src/App.tsx
@@ -1,26 +1,48 @@
 import './App.css';
 
-import React from 'react';
+import type { ChangeEvent } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import logo from './logo.svg';
+import type { Monster } from './types/monsters';
 
-function App(): JSX.Element {
+export function App(): JSX.Element {
+  const [search, setSearch] = useState<string>('');
+  let [monsters, setMonsters] = useState<Monster[]>([]);
+
+  if (search !== '') {
+    monsters = monsters.filter(monster => {
+      return monster.name.toLowerCase().includes(search.toLowerCase());
+    });
+  }
+
+  useEffect(() => {
+    const getMonsters = async (): Promise<void> => {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      const data = (await response.json()) as unknown as Monster[];
+      setMonsters(data);
+    };
+
+    getMonsters().catch(error => {
+      console.error(error);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img alt="logo" className="App-logo" src={logo} />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        className="searchBox"
+        placeholder="Search Monsters"
+        type="search"
+        value={search}
+        onChange={(event: ChangeEvent<HTMLInputElement>): void => {
+          setSearch(event.target.value);
+        }}
+      />
+      {monsters.map(monster => {
+        return <h1 key={monster.id}>{monster.name}</h1>;
+      })}
     </div>
   );
 }
