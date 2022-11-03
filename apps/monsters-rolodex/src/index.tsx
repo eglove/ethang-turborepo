13d9f05b139abe5a80f3c2af013cf6e258f92576
Ethan Glover <hello@ethang.email>
2022-11-03 14:49:27 -0500

    lint fix

--- apps/monsters-rolodex/src/index.tsx
+++ apps/monsters-rolodex/src/index.tsx
@@ -3,11 +3,11 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
+import App from './app';
 import reportWebVitals from './reportWebVitals';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+const root = ReactDOM.createRoot(document.querySelector('#root')!);
 root.render(
   <React.StrictMode>
     <App />
