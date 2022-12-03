ccf6f5c3954b87d9ea5a700c176fb4fe1d355430
Ethan Glover <hello@ethang.email>
2022-12-03 09:53:17 -0600

    container around error

--- apps/website/app/error.tsx
+++ apps/website/app/error.tsx
@@ -1,14 +1,18 @@
 'use client';
 import Link from 'next/link';
 
+import { Container } from './components';
+
 export default function Error(): JSX.Element {
   return (
-    <p>
-      Something went wrong! This site uses{' '}
-      <Link href="https://beta.nextjs.org/docs">
-        experimental server components with Next 13
-      </Link>
-      . Things might be wonky!
-    </p>
+    <Container>
+      <p>
+        Something went wrong! This site uses{' '}
+        <Link href="https://beta.nextjs.org/docs">
+          experimental server components with Next 13
+        </Link>
+        . Things might be wonky!
+      </p>
+    </Container>
   );
 }
