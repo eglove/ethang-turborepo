7deb7a5350cddcfa14749ba7175ebe3e1d60593a
ethang <hello@ethang.email>
2022-12-10 12:28:03 -0600

    ubuntu

--- packages/powershell-scripts/git-all.sh
+++ packages/powershell-scripts/git-all.sh
@@ -13,25 +13,23 @@ pnpm i --no-frozen-lockfile
 npx --yes browserslist@latest --update-db
 git add .
 git commit -m "$commitMessage"
-""
-lintResult=$(npx turbo lint)
-write $lintResult
 
-if [ "$lintResult" -eq 0 ]; then
+lintResult=$(npx turbo lint)
+
+if [ $lintResult -eq 0 ]; then
   stylelintResult=$(npx stylelint "**/*.module.css" --fix)
 fi
 
-if [ "$stylelintResult" -eq 0 ]; then
+if [ $stylelintResult -eq 0 ]; then
   testResult=$(npx turbo test)
 fi
 
-if [ "$testResult" -eq 0 ]; then
+if [ $testResult -eq 0 ]; then
   git add .
   git commit -m "$commitMessage {Automated Fixup}"
   npx turbo build
 fi
 
-# shellcheck disable=SC2181
-if [ "$?" -eq 0 ]; then
+if [ $? -eq 0 ]; then
   git push
 fi
