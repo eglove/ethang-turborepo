31b577e2fa4df5ded1ae6e1356b3b8be040edde1
ethang <hello@ethang.email>
2022-12-15 19:01:15 -0800

    type fix

--- apps/sterett-client/pages/_document.tsx
+++ apps/sterett-client/pages/_document.tsx
@@ -3,11 +3,9 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document(): JSX.Element {
   return (
     <Html lang="en-US">
-      {/* @ts-expect-error use as JSX */}
       <Head />
       <body>
         <Main />
-        {/* @ts-expect-error use as JSX */}
         <NextScript />
       </body>
     </Html>
