e3456a392b84abe92af6f0c5d445fb5616763976
Ethan Glover <hello@ethang.email>
2022-12-23 15:41:29 -0600

    Version Bump

--- apps/monsters-rolodex/src/app.tsx
+++ apps/monsters-rolodex/src/app.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import styles from './app.module.css';
 import { CardList } from './components/card-list/card-list';
