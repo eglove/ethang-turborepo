3167af6090643531c0030d0164fd61af28eafbd0
Ethan Glover <hello@ethang.email>
2023-01-09 17:37:29 -0600

    unused ts ignore

--- apps/sterett-client/vitest.config.ts
+++ apps/sterett-client/vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // @ts-expect-error https://github.com/vercel/next.js/blob/canary/examples/with-vitest/vitest.config.ts
   plugins: [react()],
   test: {
     environment: 'jsdom',
