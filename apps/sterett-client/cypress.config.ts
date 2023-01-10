a57f3efd559fe852fa94cf52e1dee8da6ade051e
Ethan Glover <hello@ethang.email>
2023-01-09 20:08:55 -0600

    disable video

--- apps/sterett-client/cypress.config.ts
+++ apps/sterett-client/cypress.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   component: {
     devServer: {
-      framework: "next",
-      bundler: "webpack",
+      bundler: 'webpack',
+      framework: 'next',
     },
   },
+  video: false,
 });
